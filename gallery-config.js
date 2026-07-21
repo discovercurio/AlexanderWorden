db35b6661cd09b4d3a830658892379db49d0e0aa
discovercurio <discovercurio@gmail.com>
2026-07-21 17:09:09 -0500

    Add files via upload

---
+++ gallery-config.js
@@ -0,0 +1,7 @@
+/*
+  Paste your deployed Google Apps Script web app URL below.
+  It should end with /exec, not /dev.
+*/
+window.AW_GALLERY_CONFIG = Object.freeze({
+  appsScriptUrl: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE"
+});
